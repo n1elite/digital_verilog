c4e3202da9c8fab35d2dbce3b9b72d5cc39c240e
dlatnals1 <127278577+dlatnals1@users.noreply.github.com>
2024-11-16 16:22:27 +0900

    간단한 설명추가

--- prj1_RISC_Toy/RISC_TOY.v
+++ prj1_RISC_Toy/RISC_TOY.v
@@ -190,84 +190,54 @@ module RISC_TOY (
     //     end
 	// end
 
-//ALU
-// ALU	
 module ALU (
-input signed [31:0]valA,
-input signed [31:0]valB,
-input signed [31:0]offset,
-input  [4:0]ALUop, //ALU operate determine
-input ALUdo,//ALU execution sign
-output reg signed [31:0]Result,
+    input signed [31:0] valA,   // 레지스터 A 값
+    input signed [31:0] valB,   // 레지스터 B 값
+    input signed [31:0] offset, // Immediate 값
+    input [4:0] ALUop,          // ALU 연산 코드
+    input ALUdo,                // ALU 실행 제어 신호
+    output reg signed [31:0] Result // 연산 결과
 );
-	
-always@(posedge CLK)
-case(ALUop)
+    parameter
+        ADDI = 5'b00000, ANDI = 5'b00001, ORI = 5'b00010, MOVI = 5'b00011, ADD = 5'b00100,
+        SUB = 5'b00101, NEG = 5'b00110, NOT = 5'b00111, AND = 5'b01000, OR = 5'b01001,
+        XOR = 5'b01010, LSR = 5'b01011, ASR = 5'b01100, SHL = 5'b01101, ROR = 5'b01110,
+        LD = 5'b10011, LDR = 5'b10100, ST = 5'b10101, STR = 5'b10110;
 
-// ADDI
-Result = valB + {{15{offset[16]}}, offset[16:0}}; //offset 부호확장
+    always @(*) begin
+        case (ALUop)
+            // Immediate 연산
+            ADDI: Result = valB + {{15{offset[16]}}, offset[16:0}};
+            ANDI: Result = valB & {{15{offset[16]}}, offset[16:0}};
+            ORI:  Result = valB | {{15{offset[16]}}, offset[16:0}};
+            MOVI: Result = {{15{offset[16]}}, offset[16:0}};
 
-// ANDI
-Result = valB & {{15{offset[16]}}, offset[16:0}};
+            // Register 간 연산
+            ADD:  Result = valA + valB;
+            SUB:  Result = valA - valB;
+            NEG:  Result = -valB;
+            NOT:  Result = ~valB;
+            AND:  Result = valA & valB;
+            OR:   Result = valA | valB;
+            XOR:  Result = valA ^ valB;
 
-// ORI
-Result = valB | {{15{offset[16]}}, offset[16:0}};
+            // Shift 연산
+            LSR:  Result = valA >> valB[4:0];
+            ASR:  Result = valA >>> valB[4:0];
+            SHL:  Result = valA << valB[4:0];
+            ROR:  Result = (valA >> valB[4:0]) | (valA << (32 - valB[4:0]));
 
-// MOVI
-Result = {{15{offset[16]}}, offset[16:0}};
+            // Load/Store 연산
+            LD:   Result = ALUdo ? (valB + {{15{offset[16]}}, offset[16:0}}) : {{15{offset[16]}}, offset[16:0}};
+            LDR:  Result = valB + {{15{offset[16]}}, offset[16:0}};
+            ST:   Result = ALUdo ? (valB + {{15{offset[16]}}, offset[16:0}}) : {{15{offset[16]}}, offset[16:0}};
+            STR:  Result = valB + {{15{offset[16]}}, offset[16:0}};
 
-// ADD
-Result = valA + valB;
-
-// SUB
-Result = valA - valB;
-
-// NEG
-Result = -valB;
-
-// NOT
-Result = ~valB;
-
-//AND
-Result = valA & valB;
-
-// OR
-Result = valA | valB;
-
-// XOR
-Result = valA ^ valB;
-
-// LSR (Logical Shift Right)
-Result = valA >> valB[4:0];
-
-// ASR (Arithmetic Shift Right)
-Result = valA >>> valB[4:0];
-
-// SHL (Shift Left)
-Result = valA << valB[4:0];
-
-// ROR (Rotate Right)
-Result = (valA >> valB[4:0]) | (valA << (32 - valB[4:0]));
-
-// LD
-Result = ALUdo ? (valB + {{15{offset[16]}}, offset[16:0}}) : {{15{offset[16]}}, offset[16:0}};
-
-// LDR
-Result = valB + {{15{offset[16]}}, offset[16:0}};
-
-// ST
-Result = ALUdo ? (valB + {{15{offset[16]}}, offset[16:0}}) : {{15{offset[16]}}, offset[16:0}};
-
-// STR
-Result = valB + {{15{offset[16]}}, offset[16:0}};
-
-endcase
-end
-
+            // Default case
+            default: Result = 32'b0;
+        endcase
+    end
 endmodule
-
-
-
 
 
 
