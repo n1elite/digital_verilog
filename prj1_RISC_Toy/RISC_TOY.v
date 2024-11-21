7d638938ddb57029b3da1bc3e4f98df47532a5d1
Jo-sang-hyeon <sdfhjk753@gmail.com>
2024-11-21 16:08:57 +0900

    Update RISC_TOY.v

--- prj1_RISC_Toy/RISC_TOY.v
+++ prj1_RISC_Toy/RISC_TOY.v
@@ -27,8 +27,28 @@ module RISC_TOY (
 
 
     // WRITE YOUR CODE
-
-
+`define	ADDI 	5'b00001
+`define	ORI		5'b00010
+`define	MOVI	5'b00011
+`define ADD		5'b00100
+`define SUB		5'b00101
+`define NEG		5'b00110
+`define NOT		5'b00111
+`define AND		5'b01000
+`define OR		5'b01001
+`define XOR		5'b01010
+`define LSR		5'b01011
+`define ASR		5'b01100
+`define SHL		5'b01101
+`define ROR		5'b01110
+`define BR		5'b01111
+`define BRL		5'b10000
+`define J		5'b10001
+`define JL		5'b10010
+`define LD		5'b10011
+`define LDR		5'b10100
+`define ST		5'b10101
+`define STR		5'b10110
 
 	
     /////////////////IF/////////////////
@@ -36,30 +56,44 @@ module RISC_TOY (
     reg [29:0] IF_iaddr;
     reg [4:0] IF_op;
 
-    /////////////////IF_ID/////////////////      
-    reg [4:0] FI_op; 
-    reg [31:0] FI_instr;                                        // 명령어
-    reg [29:0] FI_iaddr;                                        // 명령어 add
-    reg [31:0] FI_imm;
-    reg [31:0] FI_valA, FI_valB, FI_dest;
-    wire [4:0] FI_read_address0, FI_read_address1;
+
+    /////////////////IF_ID/////////////////
+	wire [4:0] FI_read_address0, FI_read_address1;
     wire [31:0] read_data0, read_data1;
 
-    /////////////////ID_EX/////////////////
-    reg [4:0] DE_read_address0, DE_read_address1, DE_dest;        // rega add _ regb add _ regc add _ opcode
-    reg [4:0] DE_op; 
-    reg [31:0] DE_valA, DE_valB, DE_imm;                          // FI_valA _ FI_valB _ FI_imm 
-    reg [31:0] DE_instr;                                        // 명령어
-    reg [29:0] DE_iaddr;                                        // 명령어 add
+
+	/////////////////ID/////////////////
+    reg [4:0] ID_dest;      
+    reg [4:0] ID_op; 
+    reg [31:0] ID_instr;                                        // 명령어
+    reg [29:0] ID_iaddr;                                        // 명령어 add
+    reg [31:0] ID_imm;
+    reg [31:0] ID_valA, ID_valB;
+
+	/////////////////ID_EX/////////////////
+	wire [31:0] ALU_out;
+	wire [31:0] ALU_PC;
+
+	//cond
+
+
+    /////////////////EX/////////////////
+    reg [4:0] EX_dest;        									// rega add _ regb add _ regc add _ opcode
+    reg [4:0] EX_op; 
+    reg [31:0] EX_ALU_out, EX_valB;                          // ID_valA _ ID_valB _ ID_imm 
+    reg [31:0] EX_instr;                                        // 명령어
+	reg [29:0] EX_iaddr;                                        // 명령어 add
+	reg [2:0] EX_cond;
+	reg EX_BR_enable;  // 상현 추가가
 
     
-    reg DE_we, DE_wer;                                          // DREQ가 될 녀석, IREQ가 될 녀석   (write enalbe / write enable reg)
+    reg EX_we, EX_wer;                                          // DREQ가 될 녀석, IREQ가 될 녀석   (write enalbe / write enable reg)
     
 
     /////////////////EX_MEM/////////////////
 
     reg [4:0] XM_op, XM_ra;                                     // opcode 5bit _ dest reg add
-    reg [31:0] XM_aluout, XM_rv1, XM_rv2, XM_instr;             // alu result _ FI_valA _ FI_valB _ 명령어
+    reg [31:0] XM_aluout, XM_rv1, XM_rv2, XM_instr;             // alu EX_ALU_out _ ID_valA _ ID_valB _ 명령어
     reg [29:0] XM_iaddr;                                        // IF_instr add (PC) => DADDR
 
     reg XM_we, XM_wer;
@@ -67,30 +101,28 @@ module RISC_TOY (
     /////////////////MEM_WB/////////////////
 
     reg [4:0] MW_op, MW_ra;                                     // opcode 5bit _ dest reg add
-    reg [31:0] MW_aluout, MW_rv1, MW_rv2, MW_instr, WB_instr;   // alu result _ FI_valA _ FI_valB _ 어떤 계산 _ 어떤 계산
+    reg [31:0] MW_aluout, MW_rv1, MW_rv2, MW_instr, WB_instr;   // alu EX_ALU_out _ ID_valA _ ID_valB _ 어떤 계산 _ 어떤 계산
     reg [29:0] MW_iaddr;                                        // P
     reg MW_wer, MW_we;  // 계산 결과 이어받음 이게 mem에 연결되서 wer에서 enable되면 reg에 저장하고 we에서 enable되면 mem에 저장
 
 
 
+	/////////////////PC_reg/////////////////
+    reg [29:0] PC;  // Program Counter
 
-
-    reg [31:0] PC;  // Program Counter
-    reg [31:0] next_PC;
-    
 
 
     // REGISTER FILE FOR GENRAL PURPOSE REGISTERS
     REGFILE    #(.AW(5), .ENTRY(32))    RegFile (
                     .CLK    (CLK),
                     .RSTN   (RSTN),
-	    	    .WEN    (WEN),
-	    .WA     (WA),
-	    .DI     (DI),
-	    .RA0    (FI_read_address0),
-	    .RA1    (FI_read_address1),
-	    .DOUT0  (read_data0),
-	    .DOUT1  (read_data1)
+                    .WEN    (),
+                    .WA     (),
+                    .DI     (),
+                    .RA0    (FI_read_address0),
+                    .RA1    (FI_read_address1),
+                    .DOUT0  (read_data0),
+                    .DOUT1  (read_data1)
     );
 
 
@@ -104,103 +136,237 @@ module RISC_TOY (
         if(~RSTN) begin
             IF_op <= 0;
             IF_instr <= 0;
-	    IF_iaddr <= 0;
+			IF_iaddr <= 0;
         end
-	else if (IREQ) begin
+        else begin
             IF_op <= INSTR[31:27];
             IF_instr <= INSTR;
-	    IF_iaddr <= PC[31:2];
+			IF_iaddr <= PC;
         end
         
     end
 
+	IADDR = IF_iaddr;
+
     /////////////////IF_ID/////////////////
 
-	wire ADDI = (IF_op == 5'b00000);
-	wire ANDI = (IF_op == 5'b00001);
-	wire ORI = (IF_op == 5'b00010);
-	wire MOVI = (IF_op == 5'b00011);
-	wire ADD = (IF_op == 5'b00100);
-	wire SUB = (IF_op == 5'b00101);
-	wire NEG = (IF_op == 5'b00110);
-	wire NOT = (IF_op == 5'b00111);
-	wire AND = (IF_op == 5'b01000);
-	wire OR = (IF_op == 5'b01001);
-	wire XOR = (IF_op == 5'b01010);
-	wire LSR = (IF_op == 5'b01011);
-	wire ASR = (IF_op == 5'b01100);
-	wire SHL = (IF_op == 5'b01101);
-	wire ROR = (IF_op == 5'b01110);
-	wire BR = (IF_op == 5'b01111);
-	wire BRL = (IF_op == 5'b10000);
-	wire J = (IF_op == 5'b10001);
-	wire JL = (IF_op == 5'b10010);
-	wire LD = (IF_op == 5'b10011);
-	wire LDR = (IF_op == 5'b10100);
-	wire ST = (IF_op == 5'b10101);
-	wire STR = (IF_op == 5'b10110);
+
+	assign FI_read_address0 = 
+    	(IF_op == `ADDI || IF_op == `ANDI || IF_op == `ORI || IF_op == `LD || 
+    	 IF_op == `ST || IF_op == `MOVI || IF_op == `NEG || IF_op == `NOT) ? IF_instr[26:22] :
+    	(IF_op == `ADD || IF_op == `SUB || IF_op == `AND || IF_op == `OR || 
+    	 IF_op == `XOR || IF_op == `LSR || IF_op == `ASR || IF_op == `SHL || 
+    	 IF_op == `ROR || IF_op == `BR || IF_op == `BRL) ? IF_instr[21:17] : 0;
+
+	assign FI_read_address1 = 
+	    (IF_op == `ADDI || IF_op == `ANDI || IF_op == `ORI || IF_op == `LD || IF_op == `ST) ? IF_instr[21:17] :
+    	(IF_op == `ADD || IF_op == `SUB || IF_op == `AND || IF_op == `OR || IF_op == `XOR || 
+    	 IF_op == `NEG || IF_op == `NOT || IF_op == `BR || IF_op == `BRL) ? IF_instr[16:12] :
+    	((IF_op == `LSR || IF_op == `ASR || IF_op == `SHL || IF_op == `ROR) && IF_instr[5]) ? IF_instr[16:12] :
+    	IF_instr[4:0];
 
 
-	assign FI_read_address0 = (ADDI || ANDI || ORI || LD || ST || MOVI || NEG || NOT) ? IF_instr[26:22] : 
-		(ADD || SUB || AND || OR || XOR || LSR || ASR || SHL || ROR || BR || BRL) ? IF_instr[21:17] : 0;
-	assign FI_read_address1 = (ADDI || ANDI || ORI || LD || ST) ? IF_instr[21:17] : 
-		(ADD || SUB || AND || OR || XOR || NEG || NOT || BR || BRL ) ? IF_instr[16:12] :
-		((LSR || ASR || SHL || ROR) && IF_instr[5]) ? IF_instr[16:12] :IF_instr[4:0];
-		
+
+
 
     always @(posedge CLK or negedge RSTN) begin
 		if(!RSTN) begin
-			FI_valA <= 0;
-			FI_valB <= 0;
-			FI_imm <= 0;
-			FI_dest <= 0;
-            		FI_iaddr <= 0;
-            		FI_instr <= 0;
-            		FI_op <= 0;
-			PC <= 0;
+			ID_valA <= 0;
+			ID_valB <= 0;
+			ID_imm <= 0;
+			ID_dest <= 0;
+            ID_iaddr <= 0;
+            ID_instr <= 0;
+            ID_op <= 0;
 		end else begin 
-            	FI_iaddr <= IF_iaddr;
-            	FI_instr <= IF_instr;
-            	FI_op <= IF_op;
-			if(ADDI || ANDI || ORI || LD || ST) begin //MOVI 주의
-				FI_valA <= read_data1;//R[ra]
-				FI_valB <= read_data0;//R[rb] 
-				FI_imm <= {15'b0, IF_instr[16:0]}; //상수
-				FI_dest <= IF_instr[26:22]; //ra
-			end else if(MOVI) begin 
-				FI_valB <= read_data1; //R[ra]
-				FI_imm <= {15'b0, IF_instr[16:0]}; //imm
-				FI_dest <= IF_instr[26:22]; //ra
-			end else if(ADD || SUB || AND || OR || XOR) begin 
-				FI_valA <= read_data0; //R[rb]
-				FI_valB <= read_data1; //R[rc]
-				FI_imm <= {15'b0, IF_instr[26:22]}; //ra
-				FI_dest <= IF_instr[26:22]; //ra
-			end else if(NEG || NOT) begin 
-				FI_valA <= read_data1; //R[rc] 
-				FI_valB <= read_data0; //R[ra]
-				FI_imm <= {15'b0, IF_instr[26:22]}; //ra 
-				FI_dest <= IF_instr[26:22]; //ra
-			end else if(LSR || ASR || SHL || ROR) begin 
-				FI_valA <= read_data0; //R[rb]  
-				FI_imm <= IF_instr[26:22]; //ra
-				FI_dest <= IF_instr[26:22]; //ra
-			end else if(BR) begin 
-				FI_valA <= read_data0; //R[rb]
-				FI_valB <= read_data1; //R[rc]
-				FI_imm <= IF_instr[2:0]; //cond
-			end else if(BRL) begin 
-				FI_valA <= read_data0; //R[rb]
-				FI_valB <= read_data1; //R[rc]
-				FI_imm <= IF_instr[2:0]; //cond 
-				FI_dest <= IF_instr[26:22];
-			end else if(JL || LDR || STR) begin 
-				FI_valA <= PC; // 현재 PC
-				FI_imm <= IF_instr[21:0]; //imm
-				FI_dest <= IF_instr[26:22]; //ra
-			end else if(J) begin 
-				FI_valA <= PC; // 현재 PC
-				FI_imm <= IF_instr[21:0]; //imm
+            ID_iaddr <= IF_iaddr;
+            ID_instr <= IF_instr;
+            ID_op <= IF_op;
+			case(IF_op)
+				`ADDI, `ANDI, `ORI: begin
+					ID_valA <= read_data1;//R[ra]
+					ID_valB <= read_data0;//R[rb] 
+					ID_imm <= {15{IF_instr[16]}, IF_instr[16:0]}; //상수
+					ID_dest <= IF_instr[26:22]; //ra
+				end `MOVI: begin 
+					ID_valB <= read_data1; //R[ra]
+					ID_imm <= {15{IF_instr[16]}, IF_instr[16:0]}; //imm
+					ID_dest <= IF_instr[26:22]; //ra
+				end `ADD, `SUB, `AND, `OR, `XOR: begin
+					ID_valA <= read_data0; //R[rb]
+					ID_valB <= read_data1; //R[rc]
+					ID_imm <= {27'b0, IF_instr[26:22]}; //ra
+					ID_dest <= IF_instr[26:22]; //ra
+				end `NEG, `NOT: begin
+					ID_valA <= read_data1; //R[rc] 
+					ID_valB <= read_data0; //R[ra]
+					ID_imm <= {27'b0, IF_instr[26:22]}; //ra 
+					ID_dest <= IF_instr[26:22]; //ra
+				end `LSR, `ASR, `SHL, `ROR: begin
+					ID_valA <= read_data0; //R[rb]  
+					ID_imm <= {27'b0, IF_instr[26:22]}; //ra			수정!
+					ID_dest <= IF_instr[26:22]; //ra	
+				end `BR: begin 
+					ID_valA <= read_data0; //R[rb]
+					ID_valB <= read_data1; //R[rc]
+					ID_imm <= {29'b0,IF_instr[2:0]}; //cond    	
+				end `BRL: begin 
+					ID_valA <= read_data0; //R[rb]
+					ID_valB <= read_data1; //R[rc]
+					ID_imm <= {29'b0,IF_instr[2:0]}; //cond  	
+					ID_dest <= IF_instr[26:22];
+				end `JL, `LDR, `STR: begin
+					ID_valA <= {IF_iaddr,2{1'b0}}; // 현재 PC
+					ID_imm <= {10{IF_instr[21]},IF_instr[21:0]}; //imm
+					ID_dest <= IF_instr[26:22]; //ra
+				end `J: begin
+					ID_valA <= {IF_iaddr,2{1'b0}}; // 현재 PC
+					ID_imm <= {10{IF_instr[21]},IF_instr[21:0]}; //imm
+				end `LD, `ST: begin
+					ID_valA <= read_data1;//R[ra]
+					ID_valB <= read_data0;//R[rb] 
+					ID_imm <= {15'b0, IF_instr[16:0]}; //상수
+					ID_dest <= IF_instr[26:22]; //ra
+				end
+			endcase
+		end
+	end
+
+
+
+
+	/////////////////ID_EX/////////////////
+	always @(*) begin
+    	case (ID_op)
+        	// Immediate 연산
+        	ADDI: ALU_out = ID_valB + {{15{offset[16]}}, offset[16:0]};
+        	ANDI: ALU_out = ID_valB & {{15{offset[16]}}, offset[16:0]};
+        	ORI:  ALU_out = ID_valB | {{15{offset[16]}}, offset[16:0]};
+        	MOVI: ALU_out = {{15{offset[16]}}, offset[16:0]};
+        	// Register 간 연산
+        	ADD:  ALU_out = ID_valA + ID_valB;
+        	SUB:  ALU_out = ID_valA - ID_valB;
+        	NEG:  ALU_out = -ID_valB;
+        	NOT:  ALU_out = ~ID_valB;
+        	AND:  ALU_out = ID_valA & ID_valB;
+        	OR :  ALU_out = ID_valA | ID_valB;
+        	XOR:  ALU_out = ID_valA ^ ID_valB;
+        	// Shift 연산
+        	LSR:  ALU_out = ID_valA >> ID_valB[4:0];   		//조건문 안나눴음 안해도 될듯?
+        	ASR:  ALU_out = ID_valA >>> ID_valB[4:0];  		//조건문 안나눴음안해도 될듯?
+        	SHL:  ALU_out = ID_valA << ID_valB[4:0];		//조건문안해도 될듯?
+        	ROR:  ALU_out = (ID_valA >> ID_valB[4:0]) | (ID_valA << (32 - ID_valB[4:0]));		//조건문안해도 될듯?
+        	BR :  begin
+				if(ID_instr[2:0] == 0) begin
+				end
+				else if (ID_instr[2:0] == 1)begin
+					ALU_PC <= ID_valA;
+					EX_BR_enable <= 1;
+				end
+				else if (ID_instr[2:0] == 2)begin
+					if(ID_valB == 0)begin
+						ALU_PC <= ID_valA;
+						EX_BR_enable <= 1;
+					end
+				end
+				else if (ID_instr[2:0] == 3)begin
+					if(ID_valB != 0)begin
+						ALU_PC <= ID_valA;
+						EX_BR_enable <= 1;
+					end
+				end
+				else if (ID_instr[2:0] == 4)begin
+					if(ID_valB >= 0)begin
+						ALU_PC <= ID_valA;
+						EX_BR_enable <= 1;
+					end
+				end
+				else if (ID_instr[2:0] == 5)begin
+					if(ID_valB < 0)begin
+						ALU_PC <= ID_valA;
+						EX_BR_enable <= 1;
+					end
+				end
+			end
+        	BRL:	begin
+				ALU_out = ID_iaddr;
+				if(ID_instr[2:0] == 0) begin
+				end
+				else if (ID_instr[2:0] == 1)begin
+					ALU_PC <= ID_valA;
+					EX_BR_enable <= 1;
+				end
+				else if (ID_instr[2:0] == 2)begin
+					if(ID_valB == 0)begin
+						ALU_PC <= ID_valA;
+						EX_BR_enable <= 1;
+					end
+				end
+				else if (ID_instr[2:0] == 3)begin
+					if(ID_valB != 0)begin
+						ALU_PC <= ID_valA;
+						EX_BR_enable <= 1;
+					end
+				end
+				else if (ID_instr[2:0] == 4)begin
+					if(ID_valB >= 0)begin
+						ALU_PC <= ID_valA;
+						EX_BR_enable <= 1;
+					end
+				end
+				else if (ID_instr[2:0] == 5)begin
+					if(ID_valB < 0)begin
+						ALU_PC <= ID_valA;
+						EX_BR_enable <= 1;
+					end
+				end
+			end
+        	J  :  ALU_PC = {ID_iaddr, 2'b0} + ID_imm;
+        	JL :  begin
+				ALU_out ={ID_iaddr, 2'b0};
+				ALU_PC = {ID_iaddr, 2'b0} + ID_imm;
+			end
+			LD : begin //수정!
+				if(ID_instr[26:22] == 5'b11111) begin				 //memory 앍어야 함  read신호
+				    ALU_out <= 15'b0, ID_imm[16:0];    
+				end	else begin
+				    ALU_out <= ID_imm + ID_valB;         
+				end
+			end
+			LDR	: ALU_out <= {ID_iaddr, 2'b0} + ID_imm;        	//memory 앍어야 함  read신호
+			ST	: begin												//memory에 적어야 함 write 신호
+				if(instr21_17 == 5'b11111) begin	
+    				ALU_out <= 15'b0, ID_imm[16:0];
+				end	else begin
+				    ALU_out <= ID_imm + ID_valB;
+				end
+			end
+			STR : ALU_out <= {ID_iaddr, 2'b0} + ID_imm; 			//memory에 적어야 함 write 신호
+		endcase			
+	end      
+
+
+	always @(posedge CLK or negedge RSTN) begin
+		if(!RSTN) begin
+			EX_dest <= 0;
+			EX_op <= 0;
+			EX_ALU_out <= 0;
+			EX_valB <= 0;
+			EX_imm <= 0;
+            EX_iaddr <= 0;
+            EX_instr <= 0;
+		end else begin
+			EX_dest <= ID_dest;
+			EX_op <= ID_op;
+			EX_ALU_out <= ALU_out;
+			EX_valB <= ID_valB;
+            
+			EX_iaddr <= ID_iaddr;
+            EX_instr <= ID_instr;
+			if (ID_op == `BR || ID_op == `BRL || ID_op == `J || ID_op == `JL)	begin
+				EX_iaddr <= ALU_PC;
+			end	else begin
+				EX_iaddr <= ID_iaddr;
 			end
 		end
 	end
@@ -208,51 +374,22 @@ module RISC_TOY (
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/////////////////////////////clock 여기 변수 수정해야 됨
-
-    always @(posedge CLK or negedge RSTN) begin
-         	if (!RSTN) begin
-              		next_PC = 0;
-           	end else if (J || JL) begin
-    		next_PC = PC + FI_imm; // Jump with FI_imm
-           	end else if (BR || BRL) begin
-    			if (FI_valA == FI_valB) begin  // ALU에서 비교한 리턴값 보내주면 조건 대체
-            			next_PC = read_data0; // Branch if condition is met
-                end else begin
-           		next_PC = PC + 4; // Increment to next IF_instr
-           	end
-        	end else begin
-        	next_PC = PC + 4; // Default: increment by 4 (next IF_instr)
-       		end
-    		end
-    end
-	
     always @(posedge CLK or negedge RSTN) begin
     	if (!RSTN) begin
         	PC <= 0;
+    	end else if (J || JL) begin
+        	PC <= PC + ID_imm; // Jump with ID_imm
+    	end else if (BR || BRL) begin
+		if (EX_BR_enable) begin
+            		PC <= read_data0; // Branch if condition is met
+        	end else begin
+            		PC <= PC + 4; // Increment to next IF_instr
+        	end
     	end else begin
-        	PC <= next_PC;
+        	PC <= PC + 4; // Default: increment by 4 (next IF_instr)
     	end
     end
 
 
 endmodule
+
